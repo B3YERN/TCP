8c81da68358a8c028aec050294da627197ebc67e
B3YERN <36043144+B3YERN@users.noreply.github.com>
2018-05-07 22:09:39 +0400

    Create TCP.py

---
+++ TCP.py
@@ -0,0 +1,11 @@
+ #IMPORTS
+import socket
+import os
+import builtins
+
+def main():
+    s = socket.socket(socket.AF_INET, sock.SOCK_STREAM)
+    host = socket.gethostbyname(socket.gethostname())
+    print(str(host))
+
+main()
